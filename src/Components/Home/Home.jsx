1ef80d35b4086477790bf0796c9e79f8ba31c0a1
Anuj192006 <anuju712@gmail.com>
2025-04-21 17:54:46 +0530

    commit changes

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate,Link } from "react-router-dom";
 import "./Home.css";
 import Navbar from "../Navbar/Navbar";
 import HomeCard from "./HomeCard";
@@ -34,13 +34,25 @@ const Home = () => {
       },
     });
   };
-
+  const arr=["Qr-Code","Todo List","Fitness","Rephraser AI","Mail AI"]
+  const arr2=["/qrcode-gen","/todo-list","/fitness","/refraser-ai","/mail-ai"]
   return (
     <div>
       <Navbar />
       <h1>Welcome to CorpPal</h1>
-
       <p style={{fontSize:"18px"}}>This is your Corperate Life friend you can have many tools to make your life easier here!</p>
+      <ul className="Tool-group">
+        {arr.map((ele, idx) => (
+          <li className="Tool-card" key={idx}>
+          <Link to={arr2[idx]}>
+            <img src='image copy.png' alt={`${ele}`} />
+            <h1>{ele}</h1>
+            <p>Click here to use {ele} tool</p>
+            </Link>
+          </li>
+        )
+        )}
+      </ul>
       <button onClick={News} disabled={result.length > 0 || loading}>
         {loading ? "Loading..." : result.length > 0 ? "News Already Loaded" : "Generate News Here"}
       </button>
